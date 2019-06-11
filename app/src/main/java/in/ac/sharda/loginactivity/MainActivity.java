f4868f85cd3a0b1e7b4a60e4a00d67f1dc970d67
SidM9 <43111742+Sidm9@users.noreply.github.com>
2019-06-11 15:50:26 +0530

    Displaying Data

--- app/src/main/java/in/ac/sharda/loginactivity/MainActivity.java
+++ app/src/main/java/in/ac/sharda/loginactivity/MainActivity.java
@@ -5,13 +5,21 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -20,13 +28,17 @@ EditText editTextUserName,textPassword;
 Button btnSubmit;
 AsyncHttpClient client;
 RequestParams params;
+ListView list;
+ArrayList listdata;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editTextUserName=(EditText)findViewById(R.id.editTextUserName);
         textPassword=(EditText)findViewById(R.id.editTextPassword);
+        list=(ListView)findViewById(R.id.list);
         btnSubmit=(Button)findViewById(R.id.btnSubmit);
+        listdata=new ArrayList();
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +58,29 @@ RequestParams params;
                         public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                             String data=new String(responseBody);
                             Log.d("arvind",data);
+try {
+    JSONArray array=new JSONArray(data);
+    for (int i=0;i<array.length();i++)
+    {
+        JSONObject jsnobj=array.getJSONObject(i);
+        int userId=jsnobj.getInt("userId");
+        int id=jsnobj.getInt("id");
+        String title=jsnobj.getString("title");
+        String body=jsnobj.getString("body");
+        listdata.add(userId+"\n"+id+"\n"+title+"\n"+body);
+
+        ArrayAdapter adapter=new ArrayAdapter(MainActivity.this,
+                android.R.layout.simple_list_item_1,listdata);
+        list.setAdapter(adapter);
+
+    }
+
+
+}catch (JSONException je)
+{
+    je.printStackTrace();
+}
+
                         }
 
                         @Override
